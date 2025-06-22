11bbf621b67de441d81da1a6f9594d2e0b84bbf9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-23 05:03:03 +0900

    Update Insertion-Sort---Part-1.cpp

--- Insertion-Sort---Part-1.cpp
+++ Insertion-Sort---Part-1.cpp
@@ -7,54 +7,22 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 
-template <typename T>
-void show_value(const T& arr)
-{
-    for (auto element : arr)
-    {
-        cout << element << ' ';
-    }
-    
+void show_value(const vector<int>& arr) {
+    for (int v : arr)
+        cout << v << ' ';
     cout << '\n';
 }
 
+void insertionSort1(int n, vector<int> arr) {
+    int key = arr[n - 1];
+    int j = n - 2;
 
-void insertionSort1(int n, vector<int> arr)
-{
-    for (int i = 1; i < n; ++i)
-    {
-        int key = arr[i];
-        int j;
-
-        for (j = i-1; j >= 0 && arr[j] > key; --j)
-        {
-            arr[j+1] = arr[j];
-            show_value(arr);
-        }
-
-        arr[j+1] = key;
+    while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        show_value(arr);
+        j--;
     }
-    
+
+    arr[j + 1] = key;
     show_value(arr);
 }
-
-
-template <typename T>
-void insertion_sort(T& arr)
-{
-	int n = arr.size();
-
-	for (int i = 1; i < n; ++i)
-	{
-		int key = arr[i];
-		int j;
-
-		for (j = i-1; j >= 0 && arr[j] > key; --j)
-		{
-			arr[j+1] = arr[j];
-		}
-
-		arr[j+1] = key;
-	}
-}
-
