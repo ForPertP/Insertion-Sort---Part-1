b680836e6b7bc2fbb75828ae096d8dc7c78a11ed
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-27 07:44:43 +0900

    Update Insertion-Sort---Part-1.cs

--- Insertion-Sort---Part-1.cs
+++ Insertion-Sort---Part-1.cs
@@ -1,1 +1,35 @@
-Insertion-Sort---Part-1
+
+class Result
+{
+
+    /*
+     * Complete the 'insertionSort1' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    static void ShowValue(List<int> arr)
+    {
+        foreach (int v in arr)
+            Console.Write(v + " ");
+        Console.WriteLine();
+    }
+
+    public static void insertionSort1(int n, List<int> arr)
+    {
+        int key = arr[n - 1];
+        int j = n - 2;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            ShowValue(arr);
+            j--;
+        }
+
+        arr[j + 1] = key;
+        ShowValue(arr);
+    }
+}
