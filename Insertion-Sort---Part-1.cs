0a436105dd7e839a09559c042bcff82f1575aed7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-27 07:46:02 +0900

    Update Insertion-Sort---Part-1.cs

--- Insertion-Sort---Part-1.cs
+++ Insertion-Sort---Part-1.cs
@@ -1,3 +1,16 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 class Result
 {
@@ -31,5 +44,18 @@ class Result
 
         arr[j + 1] = key;
         ShowValue(arr);
+    }
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.insertionSort1(n, arr);
     }
 }
